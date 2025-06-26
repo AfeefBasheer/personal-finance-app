47bfa848106c75f0d10ebcfa9ff89d70ad68a318
Afeef Basheer <afeef@example.com>
2025-06-26 17:50:35 +0530

    companyRawData Schema Basic Test fields added

--- dataProcessor/rawDataModel.js
+++ dataProcessor/rawDataModel.js
@@ -1,1 +1,17 @@
+import mongoose from "mongoose";
 
+const rawDataSchema = new mongoose.Schema({
+  companyName: String,
+  companyID: String,
+  sector:String,
+  financialYear:Number,
+  marketCapital:Number,
+  priceOfStock:Number,
+  profit:Number,
+  EPS:Number,
+  outstandingShares:Number,
+});
+
+const rawData = new mongoose.model('rawData',rawDataSchema)
+
+export default rawData
---
+++ database/database.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const database = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/shopping");
+    console.log("MongoDB Connection Successful");
+  } catch (e) {
+    console.error("MongoDB Connection Failed", e);
+    process.exit(1)
+  }
+};
+
+export default database
