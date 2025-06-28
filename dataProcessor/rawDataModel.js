994c251dd47e10759cf33afb8ca59e32acdfaeda
Afeef Basheer <afeef@example.com>
2025-06-28 12:09:08 +0530

    added rawData Schema, Json files gitignore and database connection

--- dataProcessor/rawDataModel.js
+++ dataProcessor/rawDataModel.js
@@ -1,1 +1,22 @@
+import mongoose from "mongoose";
 
+const rawDataSchema = new mongoose.Schema(
+  {
+    companyName: { type: String, required: true },
+    companyID: { type: String, required: true },
+    sector: { type: String, required: true },
+    financialYear: { type: Number },
+    marketCapital: { type: Number },
+    stockPrice: { type: Number },
+    profit: { type: Number },
+    EPS: { type: Number },
+    equity: { type: Number },
+    outstandingShares: { type: Number },
+  },
+  {
+    timestamps: true,
+  }
+);
+const RawData = mongoose.model("RawData", rawDataSchema);
+
+export default RawData;
