7efa70d23647620594c4e457473c4c06880c04be
Afeef Basheer <afeef@example.com>
2025-07-01 21:06:40 +0530

    run Script added and concised the file rawDataModel

--- dataProcessor/rawDataModel.js
+++ dataProcessor/rawDataModel.js
@@ -5,18 +5,16 @@ const rawDataSchema = new mongoose.Schema(
     companyName: { type: String, required: true },
     companyID: { type: String, required: true },
     sector: { type: String, required: true },
-    financialYear: { type: Number },
-    marketCapital: { type: Number },
-    stockPrice: { type: Number },
-    profit: { type: Number },
-    EPS: { type: Number },
-    equity: { type: Number },
-    outstandingShares: { type: Number },
+    financialYear: Number,
+    marketCapital: Number,
+    stockPrice: Number,
+    profit: Number,
+    EPS: Number,
+    equity: Number,
+    outstandingShares: Number,
   },
   {
     timestamps: true,
   }
 );
-const RawData = mongoose.model("RawData", rawDataSchema);
-
-export default RawData;
+export default mongoose.model("RawData", rawDataSchema);
