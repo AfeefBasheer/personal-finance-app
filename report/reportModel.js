0ca57e3192cda5a8187f4971c8b94352e8c5f4e8
Afeef Basheer <afeef@example.com>
2025-07-06 12:10:54 +0530

    updated the schema field :object to schema field : number and exporting

--- report/reportModel.js
+++ report/reportModel.js
@@ -5,18 +5,18 @@ const reportSchema = new mongoose.Schema(
     companyName: { type: String, required: true },
     companyID: { type: String, required: true },
     sector: { type: String, required: true },
-    financialYear: { type: Number },
-    debt: { type: Number },
-    EBITDA: { type: Number },
-    ROCE: { type: Number },
-    PE: { type: Number },
+    financialYear: { type: Number,required:true },
+    debt: Number,
+    EBITDA: Number,
+    ROCE: Number,
+    PE: Number,
   },
   {
     timestamps: true,
   }
 );
 
-const Report = mongoose.model("Report", reportSchema);
+export default mongoose.model("Report", reportSchema);
 
-export default Report;
+
 
