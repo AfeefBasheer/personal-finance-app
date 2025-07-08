b8489f7ed7ea0210cbbcc0eb2714c1eebe5b62c1
Afeef Basheer <afeef@example.com>
2025-07-08 14:30:04 +0530

    formatted the code

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import express from 'express'
-import http from 'http'
+import express from "express";
+import http from "http";
 
-const app = express()
-app.get('/health',(req,res)=>{
-    res.send('The server is healthy and running')
-})
-const server = http.createServer(app)
-app.listen(3000,()=>{
-    console.log('server running on port 3000')
-})
+const app = express();
+http.createServer(app);
+
+app.get("/health", (req, res) => {
+  res.send("The server is healthy");
+});
+app.listen(3000, () => {
+  console.log("server running on port: 3000");
+});
