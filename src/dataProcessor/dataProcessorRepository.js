cf20a4baf26a1c3259680ab303ca76861e09a273
Afeef Basheer <afeef@example.com>
2025-07-24 21:46:36 +0530

    Moved function Defintions to Repositoryfile

--- src/dataProcessor/dataProcessorRepository.js
+++ src/dataProcessor/dataProcessorRepository.js
@@ -1,2 +1,31 @@
 import rawDataModel from "./rawDataModel";
 import processedDataModel from "./processedDataModel";
+
+
+let getDebt = (
+  currentLeasePayable,
+  currentOtherFinancialLiabilities,
+  nonCurrentLeasePayable,
+  nonCurrentOtherFinancialLiabitlies
+) =>
+  currentLeasePayable +
+  currentOtherFinancialLiabilities +
+  nonCurrentOtherFinancialLiabitlies +
+  nonCurrentLeasePayable;
+
+let getEBIT = (profit, interestIncome, interestExpense, tax) =>
+  profit + tax + interestExpense - interestIncome;
+
+let getCaptialEmployed = (debt, equity) => debt + equity;
+
+let getROCE = (EBIT, captialEmployed) => EBIT / captialEmployed;
+
+let getPERatio = (stockPrice, EPS) => stockPrice / EPS;
+
+export default {
+    getDebt,
+    getEBIT,
+    getPERatio,
+    getCaptialEmployed,
+    getROCE
+}
--- src/dataProcessor/dataProcessorService.js
+++ src/dataProcessor/dataProcessorService.js
@@ -1,46 +1,29 @@
+import dataProcessorRepository from './dataProcessorRepository'
 
-let getDebt = (
-  currentLeasePayable,
-  currentOtherFinancialLiabilities,
-  nonCurrentLeasePayable,
-  nonCurrentOtherFinancialLiabitlies
-) =>
-  currentLeasePayable +
-  currentOtherFinancialLiabilities +
-  nonCurrentOtherFinancialLiabitlies +
-  nonCurrentLeasePayable;
-
-let getEBIT = (profit, interestIncome, interestExpense, tax) =>
-  profit + tax + interestExpense - interestIncome;
-
-let getCaptialEmployed = (debt, equity) => debt + equity;
-
-let getROCE = (EBIT, captialEmployed) => EBIT / captialEmployed;
-
-let getPERatio = (stockPrice, EPS) => stockPrice / EPS;
 
 function processRawData(rawData) {
   let processedData = {};
-  processedData.debt = getDebt(
+  processedData.debt = dataProcessorRepository.getDebt(
     rawData.currentLeasePayable,
     rawData.currentOtherFinancialLiabilities,
     rawData.nonCurrentLeasePayable,
     rawData.nonCurrentOtherFinancialLiabitlies
   );
-  processedData.EBIT = getEBIT(
+  processedData.EBIT = dataProcessorRepository.getEBIT(
     rawData.profit,
     rawData.interestIncome,
     rawData.interestExpense,
     rawData.tax
   );
-  processedData.captialEmployed = getCaptialEmployed(
+  processedData.captialEmployed = dataProcessorRepository.getCaptialEmployed(
     processedData.debt,
     rawData.equity
   );
-  processedData.ROCE = getROCE(
+  processedData.ROCE = dataProcessorRepository.getROCE(
     processedData.EBIT,
     processedData.captialEmployed
   );
-  processRawData.PE = getPERatio(rawData.stockPrice, rawData.EPS);
+  processRawData.PE = dataProcessorRepository.getPERatio(rawData.stockPrice, rawData.EPS);
   return processedData;
 }
+
