dd96de8cea2eeda257ffed9be8c2c789b4eee061
Afeef Basheer <afeef@example.com>
2025-09-06 06:53:15 +0530

    Updated the status code send mechanism

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -4,8 +4,10 @@ import dataProcessorService from "../dataProcessor/dataProcessorService.js";
 const Router = express.Router();
 
 Router.post("/rawdata", async (req, res) => {
-    let response = await dataProcessorService.addRawData(req.body);
-    res.send(response);
+  let response = await dataProcessorService.addRawData(req.body);
+  if (!response) res.status(500).send();
+  else res.status(201).send(response);
+  
 });
 
 export default Router;
